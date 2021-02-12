31d2919609e1722dc50adf6fbfd2c82700c5daa7
Frank Benicio <frank.benicio@infouniron.com>
2021-02-12 00:01:56 -0400

    [TSK] Api Controller
    [NEW] Add  upload api for file

---
+++ ExcelImportWebApi/ExcelImportWebApi/Controllers/ExcelController.cs
@@ -0,0 +1,45 @@
+﻿using Histories.ProductHistory;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace ExcelImportWebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ExcelController : ControllerBase
+    {
+        [HttpPost("upload")]
+        public async Task<IActionResult> Post([FromServices] ListProducts listProducts, [FromServices] ReadExcelSpreadsheet readExcelSpreadsheet, [FromForm] IFormFile file)
+        {
+            try
+            {
+                await readExcelSpreadsheet.Run(file);
+
+                var list = await listProducts.Run();
+
+
+                var response = new
+                {
+                    success = true,
+                    products = list
+
+                };
+
+                return new ObjectResult(response);
+            }
+            catch (System.Exception e)
+            {
+
+                var response = new
+                {
+                    success = false,
+                    message = e.Message
+
+                };
+
+                return new ObjectResult(response);
+            }
+        }
+    }
+}
